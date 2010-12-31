69fc47a5da2789159359403fc62493c3b75fedc3
Ken Stanley <ken@500sw.com>
2010-12-31 15:42:02 -0500

    Fisrt commit just the Hello World

---
+++ src/com/example/helloandroid/HelloWorldActivity.java
@@ -0,0 +1,17 @@
+package com.example.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelloWorldActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+       tv.setText("Hello, Peter say Hi to your family");
+       setContentView(tv);
+    /**        setContentView(R.layout.main);   */
+    }
+}
